410ea7ff26ee3867d6487daf17d1fa50018d9f6a
Russell M  Martin <rmmartin@melkor.cs.wm.edu>
2019-03-13 12:35:36 -0400

    initial commit

---
+++ sim.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from math import log2
+
+addressSize = 4 #bytes
+blocks = 1
+blockSize = blocks*addressSize
+ways = 1
+setSize = 256
+cacheSize = blockSize * setSize * ways
+
+cache = []
+for s in range(setSize):
+	cache.append([])
+	for w in range(ways):
+		cache[s].append([0]*int(blockSize/addressSize))
+
+print(cache)
+print(setSize)
+
+with open('Traces/433.milc') as f:
+	trace = f.readlines()
+
+print(len(trace))
+#trace = trace[:100]
+sets = [0]*len(trace)
+
+blockBits = int(log2(blocks))
+setBits = int(''.join(['1']*int(log2(setSize))),2) << int(log2(blocks))
+print(format(blockBits, "b"),format(setBits, "b"))
+
+for t in range(len(trace)):
+	a = int(trace[t].split(' ')[1])
+	print('Address',format(a, "b"))
+	b = a & blockBits
+	s = a & setBits
+	sets[t] = s
+
+plt.hist(sets,setSize)
+plt.show()
